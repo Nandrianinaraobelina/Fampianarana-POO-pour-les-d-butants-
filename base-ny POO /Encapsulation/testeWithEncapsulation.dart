74ae5e1b8393f9fb9caec7d02e026fad56ea575a
herynandrianina <herynandrianina@gmail.com>
2026-05-02 22:57:57 +0300

    docs : ajout coms

--- base-ny POO /Encapsulation/testeWithEncapsulation.dart
+++ base-ny POO /Encapsulation/testeWithEncapsulation.dart
@@ -2,7 +2,7 @@ class Personne{
   String _nom;
   int _age;
   //eto ny constructeur
-  Personne(this._nom ,int age) : _age = (age >= 0) ? age : 0;
+  Personne(this._nom ,int age) : _age = (age >= 0) ? age : 0;  //eto condition mi verifie hoe negative sa positive
 
   //miditra getter : LECTURE
   String get nom => _nom;
