329d6059616cab2d77ba00324dded027ebb184a9
herynandrianina <herynandrianina@gmail.com>
2026-05-02 14:23:32 +0300

    feat: difference entre les deux constructeurs

--- base-ny POO /class/teste.dart
+++ base-ny POO /class/teste.dart
@@ -2,14 +2,28 @@ class Personne{
   String nom = "";
   int age =0;
 
+
+  //fomba fi-creena constructeur
+  Personne(
+    this.nom,
+    this.age
+  );
+
   void parler(){
     print("Ny anarako dia $nom ary $age taona aho");
   }
 }
 void main(){
-  Personne p = new Personne();
+  //fomba taloha
+  
+  /*Personne p = new Personne();
   p.nom = "Nandrianina";
   p.age = 24;
+  p.parler();*/
 
+
+  //fomba vaovao rehefa misy constructeur
+  var p = Personne("nandrianina",24);
   p.parler();
+
   }
