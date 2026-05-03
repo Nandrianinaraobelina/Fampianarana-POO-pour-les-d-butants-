48f5c343672d5341bf31cac9dd3840a183a7fd2b
herynandrianina <herynandrianina@gmail.com>
2026-05-03 16:00:36 +0300

    feat : ajout encapsulation

--- base-ny POO /heritage/teste.dart
+++ base-ny POO /heritage/teste.dart
@@ -14,9 +14,20 @@ class Boeuf{
 //raha misy heritage
 
 class Animal{
-  String nom;
+  String _nom;
   
-  Animal(this.nom);
+  Animal(this._nom);
+
+  String get nom => _nom;
+
+   set nom(String nom){
+    if(nom.isNotEmpty){
+      _nom = nom;
+    }
+    else{
+      print("sans caracterers");
+    }
+  }
 
   void manger(){
     print("Mon $nom est manger");
@@ -26,27 +37,43 @@ class Chien extends Animal{
     Chien(String nom)  : super(nom){
       print("Chien crée");
     }
+    void mivovo(){
+      print("wouf wouf Wouf");
+    }
   }
-
 class Chat  extends Animal{
     Chat(String nom) : super(nom){
       print("Chat crée aussi");
     }
+    void miammm(){
+      print("MIANNN miannn");
+    }
   }
-
 class Boeuf extends Animal{
     Boeuf(String nom) : super(nom){
       print("boeuf crée");
     }
+    void hummm(){
+      print("MOMMMMMMMMM");
+    }
   }
 
 void main(){
-    Animal a = Animal("boby");
-    a.manger();
+    Chien ch = Chien("boby");
+    ch.manger();
+    ch.mivovo();
 
     Chat c = Chat("Piso");
     c.manger();
+    c.miammm();
 
     Boeuf b = Boeuf("Ngilo");
     b.manger();
+    b.hummm();
+    
+    //eo @ilay encapsulation
+    c.nom = "sasa";
+    c.nom = "" //miatra le set any ambiony rah vide ity
+    c.manger();
+
 }
